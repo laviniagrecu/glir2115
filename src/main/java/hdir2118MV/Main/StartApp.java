2dbc343512a89565d1a623da69eb8d0426568b74
Denisa Holhos - (p) <denisa.holhos@telenav.com>
2019-03-18 21:24:56 +0200

    Laborator 1 - Coding phase defects checklist - improvements done

---
+++ src/main/java/hdir2118MV/Main/StartApp.java
@@ -0,0 +1,74 @@
+package hdir2118MV.Main;
+
+import hdir2118MV.Controllers.NoteController;
+import hdir2118MV.Models.*;
+import hdir2118MV.utils.ClasaException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+//functionalitati
+//F01.	 adaugarea unei note la o anumita materie (nr. matricol, materie, nota acordata);
+//F02.	 calcularea mediilor semestriale pentru fiecare elev (nume, nr. matricol),
+//F03.	 afisarea elevilor coringenti, ordonati descrescator dupa numarul de materii la care nu au promovat ?i alfabetic dupa nume.
+
+
+public class StartApp {
+
+	/**
+	 * @param args
+	 * @throws ClasaException 
+	 */
+	public static void main(String[] args) throws ClasaException {
+		// TODO Auto-generated method stub
+		NoteController ctrl = new NoteController();
+		List<Medie> medii = new LinkedList<Medie>();
+		List<Corigent> corigenti = new ArrayList<Corigent>();
+		ctrl.readElevi(args[0]);
+		ctrl.readNote(args[1]);
+		ctrl.creeazaClasa(ctrl.getElevi(), ctrl.getNote());
+		boolean gasit = false;
+		while(!gasit) {
+			System.out.println("1. Adaugare Nota");
+			System.out.println("2. Calculeaza medii");
+			System.out.println("3. Elevi corigenti");
+			System.out.println("4. Iesire");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in) );
+		    try {
+				int option = Integer.parseInt(br.readLine());
+				switch(option) {
+				case 1:
+					int nrMatricol = Integer.parseInt(br.readLine());
+					String materie = br.readLine();
+					int value = 	Integer.parseInt(br.readLine());
+					Nota nota = new Nota(nrMatricol, materie, value);
+					ctrl.addNota(nota);
+					break;
+				case 2: medii = ctrl.calculeazaMedii();
+						for(Medie medie:medii)
+							System.out.println(medie);
+						break;
+				case 3: corigenti = ctrl.getCorigenti();
+						for(Corigent corigent:corigenti)
+							System.out.println(corigent);
+						break;
+				case 4: gasit = true;
+						break;
+				default: System.out.println("Introduceti o optiune valida!");
+				}
+				
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
