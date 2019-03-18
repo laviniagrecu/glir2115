13c7aa3ac2045a1b6b5e0912f8f5a24c15817501
Denisa Holhos - (p) <denisa.holhos@telenav.com>
2019-03-18 20:29:39 +0200

    inital commit

---
+++ src/main/java/hdir2118MV/App.java
@@ -0,0 +1,13 @@
+package hdir2118MV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
